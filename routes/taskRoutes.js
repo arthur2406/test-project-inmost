1a193ac7142e940915c2a40a63ba335dec1b489c
arthur2406 <rizhiy2406@gmail.com>
2020-07-01 20:35:23 +0300

    set up api layer

---
+++ routes/taskRoutes.js
@@ -0,0 +1,8 @@
+'use strict';
+
+const { Router } = require('express');
+const TaskService = require('../services/userService');
+
+const router = Router();
+
+module.exports = router;
---
+++ routes/userRoutes.js
@@ -0,0 +1,8 @@
+'use strict';
+
+const { Router } = require('express');
+const UserService = require('../services/userService');
+
+const router = Router();
+
+module.exports = router;
