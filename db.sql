95d64b3cd3eed5e13616cba9014afe04a71a7dee
arthur2406 <rizhiy2406@gmail.com>
2020-07-01 14:39:00 +0300

    add db implementation

---
+++ db.sql
@@ -0,0 +1,16 @@
+CREATE TABLE users (
+  user_id SERIAL PRIMARY KEY,
+  first_name VARCHAR(25) NOT NULL,
+  last_name VARCHAR(25) NOT NULL,
+  email VARCHAR(50) NOT NULL UNIQUE
+);
+
+CREATE TYPE taskStatus AS ENUM ('View', 'In Progress', 'Done');
+
+CREATE TABLE tasks (
+  id SERIAL PRIMARY KEY,
+  user_id INTEGER REFERENCES users(user_id) NOT NULL,
+  title VARCHAR(100) NOT NULL,
+  description VARCHAR(500) NOT NULL,
+  status taskStatus NOT NULL
+);
