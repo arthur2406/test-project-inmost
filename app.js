78173a869e0c6962a108a1a65d823693533f5c4f
arthur2406 <rizhiy2406@gmail.com>
2020-07-01 20:35:36 +0300

    minor

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 'use strict';
 
 const express = require('express');
+const routes = require('./routes/index');
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-//const routes = require('./routes/index');
-//routes(app);
+routes(app);
 
 module.exports = app;
