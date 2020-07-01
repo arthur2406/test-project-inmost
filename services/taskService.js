d1d56c87df807840f45c345c68645deb2bfbdc4b
arthur2406 <rizhiy2406@gmail.com>
2020-07-02 00:25:00 +0300

    add methods to service to create and update task

--- services/taskService.js
+++ services/taskService.js
@@ -1,8 +1,33 @@
 'use strict';
 
-const { UserRepository } = require('../repositories/taskRepository');
+const { TaskRepository } = require('../repositories/taskRepository');
 
 class TaskService {
+
+  handleRepositoryError(e) {
+    console.error(e);
+    throw e;
+  }
+
+  async create(data) {
+    try {
+      const task = await TaskRepository.create(data);
+      return task;
+    } catch (e) {
+      this.handleRepositoryError(e);
+    }
+  }
+
+  async update(id, data) {
+    try {
+      const task = await TaskRepository.update(id, data);
+      return task;
+    } catch (e) {
+      this.handleRepositoryError(e);
+    }
+  }
+
+
 
 }
 
