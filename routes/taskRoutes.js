ce9e31331263615953eb112be53b2cd4bbcbd4a0
arthur2406 <rizhiy2406@gmail.com>
2020-07-02 17:15:05 +0300

    refactor routes to create/update

--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -1,9 +1,11 @@
+/* eslint-disable camelcase */
 'use strict';
 
 const { Router } = require('express');
 const TaskService = require('../services/userService');
 const responseMiddleware = require('../middlewares/response.middleware');
-const auth = require('../middlewares/auth.middleware');
+const verifyAuth = require('../middlewares/verifyAuth.middleware');
+const { isEmpty } = require('../helpers/validations');
 
 const router = Router();
 
@@ -17,13 +19,31 @@ const router = Router();
 //   }
 // });
 
-router.post('/', auth, async (req, res, next) => {
+const statuses = ['View', 'In Progress', 'Done'];
+
+router.post('/', verifyAuth, async (req, res, next) => {
+  const {
+    user_id, title, description, status
+  } = req.body;
+
+  if (isEmpty(title) || isEmpty(description) || isEmpty(status)) {
+    res.status(400);
+    next(new Error('Title, description and status cannot be empty'));
+  }
+
+  if (!statuses.includes(status)) {
+    res.status(400);
+    next(new Error('Status should be "View", "In progress" or "Done"'));
+  }
+
   try {
-    const task = await TaskService.create({
-      // eslint-disable-next-line camelcase
-      user_id: (req.body.userId ? req.body.userId : req.user.id),
-      ...req.body,
-    });
+    const body = {
+      title,
+      description,
+      status
+    };
+    body[user_id] = user_id ? user_id : req.user.user_id;
+    const task = await TaskService.create(body);
     res.data = task;
     res.status(201);
   } catch (e) {
@@ -35,19 +55,39 @@ router.post('/', auth, async (req, res, next) => {
 
 // PUT /api/tasks/{id}?status={status}
 
-router.put('/:id', auth, async (req, res, next) => {
+router.put('/:id', verifyAuth, async (req, res, next) => {
+  const updates = req.body;
+  const allowedUpdates = ['title', 'description', 'status'];
+  const isValidOperation = Object.keys(updates).every(update => allowedUpdates.includes(update));
+  if (!isValidOperation) {
+    res.status(400);
+    next(new Error('Invalid updates'));
+  }
+
+  for (const val of Object.values(updates)) {
+    if (isEmpty(val)) {
+      res.status(400);
+      next(new Error('Any field cannot be empty'));
+    }
+  }
+
+  if (updates.status && !status.includes(updates.status)) {
+    res.status(400);
+    next(new Error('Status should be "View", "In progress" or "Done"'));
+  }
+
   try {
     if (req.query.status) {
       req.body.status = req.query.status;
     }
-    req.body.userId = req.user.id;
+    req.body.user_id = req.user.user_id;
     const task = await TaskService.update(req.params.id, req.body);
     if (task) {
       res.data = task;
       res.status(200);
     } else {
       res.status(404);
-      next(new Error('User not found'));
+      next(new Error('Task not found'));
     }
   } catch (e) {
     next(e);
@@ -56,15 +96,15 @@ router.put('/:id', auth, async (req, res, next) => {
   next();
 }, responseMiddleware);
 
-router.put('/:taskId/:userId', auth, async (req, res, next) => {
+router.put('/:task_id/:user_id', verifyAuth, async (req, res, next) => {
   try {
-    const task = await TaskService.updateUser(req.params.taskId, req.params.userId);
+    const task = await TaskService.updateUser(req.params.task_id, req.params.user_id);
     if (task) {
       res.data = task;
       res.status(200);
     } else {
       res.status(404);
-      next(new Error('Incorrect taskId'));
+      next(new Error('Incorrect task_id'));
     }
   } catch (e) {
     next(e);
@@ -72,9 +112,9 @@ router.put('/:taskId/:userId', auth, async (req, res, next) => {
 }, responseMiddleware);
 
 
-router.delete('/:id', auth, async (req, res, next) => {
+router.delete('/:id', verifyAuth, async (req, res, next) => {
   try {
-    const task = await TaskService.delete(req.params.id, req.user.id);
+    const task = await TaskService.delete(req.params.id, req.user.user_id);
     if (task) {
       res.data = task;
       res.status(200);
