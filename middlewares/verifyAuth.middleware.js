9e9c91e3f0e007f8c40772f28cfab0d231660a97
arthur2406 <rizhiy2406@gmail.com>
2020-07-02 17:16:51 +0300

    add check if user exists to verifyAuth

--- middlewares/verifyAuth.middleware.js
+++ middlewares/verifyAuth.middleware.js
@@ -2,28 +2,30 @@
 /* eslint-disable camelcase */
 
 const jwt = require('jsonwebtoken');
-//const UserService = require('../services/userService');
+const UserService = require('../services/userService');
 
 
 const verifyToken = async (req, res, next) => {
-  const { token } = req.headers;
-  if (!token) {
-    res.status(400);
-    next(new Error('Token not provided'));
-  }
   try {
+    const token = req.header('Authorization').replace('Bearer', '');
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const dbRow = UserService.getUserByEmail(decoded.email);
+    if (!dbRow) {
+      res.status(401);
+      next(new Error('There is no user with this email'));
+    }
     req.user = {
       email: decoded.email,
       user_id: decoded.user_id,
       first_name: decoded.first_name,
       last_name: decoded.last_name
     };
-    next();
   } catch (e) {
     res.status(401);
     next(new Error('Authentication failed'));
   }
+
+  next();
 };
 
 module.exports = verifyToken;
