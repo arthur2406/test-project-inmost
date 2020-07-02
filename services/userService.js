dee2cab4b923570d3fb63a5aecd46ac378aa404b
arthur2406 <rizhiy2406@gmail.com>
2020-07-02 17:22:42 +0300

    add update/delete funcs to user service

--- services/userService.js
+++ services/userService.js
@@ -26,6 +26,24 @@ class UserService {
       this.handleRepositoryError(e);
     }
   }
+
+  async update(userId, data) {
+    try {
+      const user = await UserRepository.update(userId, data);
+      return user;
+    } catch (e) {
+      this.handleRepositoryError(e);
+    }
+  }
+
+  async delete(userId) {
+    try {
+      const user = await UserRepository.delete(userId);
+      return user;
+    } catch (e) {
+      this.handleRepositoryError(e);
+    }
+  }
 }
 
 module.exports = new UserService();
