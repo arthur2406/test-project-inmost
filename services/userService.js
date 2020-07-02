11984d80fc5846ba3089e8812ffd71f60c778829
arthur2406 <rizhiy2406@gmail.com>
2020-07-02 15:46:10 +0300

    add create func to user service, err handler

--- services/userService.js
+++ services/userService.js
@@ -4,6 +4,20 @@ const { UserRepository } = require('../repositories/userRepository');
 
 class UserService {
 
+  handleRepositoryError(e) {
+    console.error(e);
+    throw e;
+  }
+
+  async create(data) {
+    try {
+      const user = await UserRepository.create(data);
+      return user;
+    } catch (e) {
+      this.handleRepositoryError(e);
+    }
+
+  }
 }
 
 module.exports = new UserService();
